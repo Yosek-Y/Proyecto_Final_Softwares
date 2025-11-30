ccd73c0550783db460271c68e7d728783c37933d
Joscen11 <nightmorning1572@gmail.com>
2025-11-30 18:13:37 -0500

    Servicio para crear nuevas órdenes de servicio
    * Se añadió el método CrearOrdenServicio.
    * Validación del tipo de servicio mediante Enum.IsDefined().
    * Validación del diagnóstico y costo.
    * Validación de existencia del vehículo.
    * Construcción del objeto OrdenesServicio.
    * Inserción en la BD mediante _context.OrdenesServicios.Add().
    * Retorno de la orden creada.

---
+++ Back-End/Mecario_BackEnd/Mecario_BackEnd/Servicios/OrdenesServicioServicio.cs
@@ -0,0 +1,49 @@
+﻿using Mecario_BackEnd.DBContexs;
+using Mecario_BackEnd.Modelos;
+using Mecario_BackEnd.Modelos.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+
+namespace Mecario_BackEnd.Servicios
+{
+    public class OrdenesServicioServicio
+    {
+        private readonly ContextoBD _context;
+
+        public OrdenesServicioServicio(ContextoBD context)
+        {
+            _context = context;
+        }
+        // Crear una nueva orden de servicio
+        public async Task<OrdenesServicio> CrearOrdenServicio(CrearOrdenServicioDTO dto)
+        {
+            // VALIDACIONES
+            if (!Enum.IsDefined(typeof(OrdenesServicio.TipoServicio), dto.TipoServicio))
+                throw new ArgumentException("El tipo de servicio no es válido.");
+
+            if (string.IsNullOrWhiteSpace(dto.diagnosticoInicial))
+                throw new ArgumentException("El diagnóstico es obligatorio.");
+
+            if (dto.costoInicial < 0)
+                throw new ArgumentException("El costo inicial no puede ser negativo.");
+
+            var vehiculo = await _context.Vehiculos.FindAsync(dto.idVehiculo);
+            if (vehiculo == null)
+                throw new ArgumentException("El vehículo especificado no existe.");
+
+            // CREAR OBJETO
+            var nuevaOrden = new OrdenesServicio
+            {
+                tipoServicio = (OrdenesServicio.TipoServicio)dto.TipoServicio,
+                diagnosticoInicial = dto.diagnosticoInicial,
+                costoInicial = dto.costoInicial,
+                idVehiculo = dto.idVehiculo
+            };
+
+            _context.OrdenesServicios.Add(nuevaOrden);
+            await _context.SaveChangesAsync();
+
+            return nuevaOrden;
+        }
+    }
+}
